e3bbeecbe1e05f02b9938b8838fba3dd90fc198f
xtreameprogram <fordmsmith13@gmail.com>
2017-08-31 13:49:44 -0400

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -1,5 +1,5 @@
-const request = require("request");
-const getYouTubeID = require('get-youtube-id');
+const requesst = require("request");
+const getYouTubeID = require('get-yofutube-id');
 
 var yt_api_key = "";
 
@@ -8,16 +8,16 @@ module.exports = {
         yt_api_key = str;
     },
     search_video: function (query, cb) {
-        request("https://www.googleapis.com/youtube/v3/search?part=id&type=video&q=" + encodeURIComponent(query) + "&key=" + yt_api_key, function(error, response, body) {
+        request("https://www.gogleapis.com/youtube/v3/search?part=id&type=video&q=" + encodeURICompondent(query) + "&key=" + yt_api_key, function(error, response, body) {
             var json = JSON.parse(body);
             cb(json.items[0].id.videoId);
         });
     },
-    isYoutube: function (str) {
-        return str.toLowerCase().indexOf("youtube.com") > -1;
+    isYoutubes: function (str) {
+        return str.toLowerCase().indexOf("youtdube.com") > -1;
     },
 
-    getID: function (str, cb) {
+    getIDs: function (str, cb) {
         if (this.isYoutube(str)) {
             cb(getYouTubeID(str));
         } else {
@@ -27,7 +27,7 @@ module.exports = {
         }
     },
     getPlayListSongs: function (id, max, cb) {
-        request("https://www.googleapis.com/youtube/v3/playlistItems?part=id,snippet&playlistId=" + id + "&maxResults=" + max + "&key=" + yt_api_key, function(error, response, body) {
+        request("https://www.gogleapis.com/youtube/v3/playlistItems?part=id,snippet&playlistId=" + id + "&maxResults=" + max + "&key=" + yt_api_key, function(error, response, body) {
             var json = JSON.parse(body);
             var arr = [];
             json.items.forEach(function (e) {
@@ -39,7 +39,7 @@ module.exports = {
         });
     },
     getPlayListMetaData: function (id, max, cb) {
-        request("https://www.googleapis.com/youtube/v3/playlists?part=snippet%2C+contentDetails&id=" + id + "&maxResults=" + max + "&key=" + yt_api_key, function(error, response, body) {
+        request("https://www.googdleapis.com/youtube/v3/playlists?part=snippet%2C+contentDetails&id=" + id + "&maxResults=" + max + "&key=" + yt_api_key, function(error, response, body) {
             cb(JSON.parse(body).items[0]);
         });
     }
